044dcd59ae60da199c7b18476408c14027df316e
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-09-12 08:16:35 +0300

    done

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,50 +1,132 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import "./Card.css";
 import man from "./../../assets/man.svg";
+import woman from "./../../assets/woman.svg";
 import mail from "./../../assets/mail.svg";
 import growingUpMan from "./../../assets/growingUpMan.svg";
 import growingUpWoman from "./../../assets/growingUpWoman.svg";
 import map from "./../../assets/map.svg";
 import phone from "./../../assets/phone.svg";
 import padlock from "./../../assets/padlock.svg";
+import axios from "axios"
+import Table from "../table/Table";
+// import Table from "../table/Table";
 
 export default function Card() {
 
-    const [heading, setHeading] = useState("");
-    const [subHeading, setSubHeading] = useState("");
-    const handleName = (heading) => setHeading("My name is");
+  const [data,setData] = useState([]);
+  const [userList,setUserList] = useState([])
+  const [heading, setHeading] = useState(["My name is",data===""  ?`${data.name.title} ${data.name.first} ${data.name.last}` : []]);
+  
+  const getUser = () => {
+    axios.get("https://randomuser.me/api/")
+    .then((res) => {
+
+      setData(res.data.results[0]);
+      setHeading(["My name is",`${res.data.results[0].name.title} ${res.data.results[0].name.first} ${res.data.results[0].name.last}`]);
+
+    })
+    
+  }
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  
+
+
+
+
+    const handleName = (heading) => setHeading(["My name is",`${data.name.title} ${data.name.first} ${data.name.last}`]);
+    const handleMail = (heading) => setHeading(["My email is",`${data.email}`]);
+    const handleAge = (heading) => setHeading(["My age is",`${data.dob.age}`]);
+    const handleStreet = (heading) => setHeading(["My street is",`${data.location.street.name}`]);
+    const handlePhone = (heading) => setHeading(["My phone is",`${data.phone}`]);
+    const handlePassword = (heading) => setHeading(["My password is",`${data.login.password}`]);
+   
+
+
+    // let tempArrForUserList = userList;
+    // console.log("userList",userList,typeof userList);
+  
+    // const addUser1 = () => {
+      
+    
+             
+
+    //           if (userList.filter((el) => el.email === data.email).length === 0) {
+
+    //             tempArrForUserList.push({
+    //                     firstName: `${data.name.first}`,
+    //                     email: `${data.email}`,
+    //                     phone: `${data.phone}`,
+    //                     age: `${data.dob.age}`
+    //                   })
+
+    //           }
+
+    //            setUserList(tempArrForUserList);
+      
+    // }
+    const addUser = ()=>{
+      if (userList.filter((el) => el.email === data.email).length === 0){
+            setUserList(oldArray => [...oldArray,{
+            firstName: `${data.name?.first}`,
+            email: `${data.email}`,
+            phone: `${data.phone}`,
+            age: `${data.dob?.age}`
+          }])
+        }
+    }
+    
   return (
     <div className="card-container">
       <div className="banner-line card_banner-line">
         <div className="card-img_container">
-          <img className="card-img" src="https://picsum.photos/50/50" alt="" />
+          <img className="card-img" src={data.picture?.large} alt="" />
         </div>
       </div>
 
       <div className="main-container">
-        <p className="card-heading">{heading}</p>
-        <p className="card-sub-heading">İsmail Aslan</p>
+        <p className="card-heading">{heading[0]}</p>
+        <p className="card-sub-heading">{heading[1]}</p>
         <div className="buttons-container">
           <div className="btn-container">
-            <img className="btn-img" src={man} alt="" onClick={handleName}/>
+            <img className="btn-img" src={data.gender==="male" ? man : woman} alt="" onMouseOver={handleName}/>
           </div>
           <div className="btn-container">
-            <img className="btn-img" src={mail} alt="" />
+            <img className="btn-img" src={mail} alt="" onMouseOver={handleMail}/>
           </div>
           <div className="btn-container">
-            <img className="btn-img" src={growingUpMan} alt="" />
+            <img className="btn-img" src={data.gender==="male" ? growingUpMan : growingUpWoman} alt="" onMouseOver={handleAge}/>
           </div>
           <div className="btn-container">
-            <img className="btn-img" src={map} alt="" />
+            <img className="btn-img" src={map} alt="" onMouseOver={handleStreet}/>
           </div>
           <div className="btn-container">
-            <img className="btn-img" src={phone} alt="" />
+            <img className="btn-img" src={phone} alt="" onMouseOver={handlePhone}/>
           </div>
           <div className="btn-container">
-            <img className="btn-img" src={padlock} alt="" />
+            <img className="btn-img" src={padlock} alt="" onMouseOver={handlePassword}/>
           </div>
         </div>
+
+        <div className="sub-buttons-container">
+            <button className="btn-user" onClick={getUser}>New User</button>
+            <button className="btn-user" onClick={addUser}>Add User</button>
+
+            
+              
+            <Table  userList={userList}></Table>
+              
+            
+            
+
+        </div>
+        
       </div>
+
+  
     </div>
   );
 }
---
+++ src/components/table/Table.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Table.css"
+
+export default function Table(props) {
+  
+  return (
+    <div>
+      {props.userList.length > 0 
+      &&
+        <table >
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.userList.map((el, i) => {
+              
+              return (
+                <tr key={i}>
+                  <td>{i+1}</td>
+                  <td>{el.firstName}</td>
+                  <td>{el.email}</td>
+                  <td>{el.phone}</td>
+                  <td>{el.age}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>}
+    </div>
+  );
+}
